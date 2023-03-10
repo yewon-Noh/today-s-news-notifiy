eb8594fe6d429413612c5e8c9006e3cba84a8dd3
yewon-Noh <yewo2nn16@gmail.com>
2023-03-11 01:42:51 +0900

    #9 selenium 오류 수정

--- src/main/java/news/today/notification/search/Driver.java
+++ src/main/java/news/today/notification/search/Driver.java
@@ -41,6 +41,7 @@ public class Driver {
         options.addArguments("headless");   // 브라우저 안띄움
         options.addArguments("--disable-gpu");  // gpu 비활성화
         options.addArguments("--blink-settings=imagesEnabled=false");   // 이미지 다운 안받음
+        options.addArguments("--remote-allow-origins=*");
 
         WebDriver driver = new ChromeDriver(options);
         return driver;
